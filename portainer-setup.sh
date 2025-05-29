7a2579a9a99c16df43f4988270166f23b13d5b34
Zero0242 <pedrocorream66@gmail.com>
2025-05-29 13:37:52 -0400

    [SERVER]
    > minecraft

--- portainer-setup.sh
+++ portainer-setup.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# This script sets up Portainer, a lightweight management UI for Docker.
+# It pulls the Portainer image and runs it in a Docker container.
 docker container run -d \
 -p 9000:9000 \
 --name portainer \
